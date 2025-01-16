910f734b04b12f924bdf4109b9187ff9f64cd1e3
Zaker Choudhury <20971850+KaBankz@users.noreply.github.com>
2025-01-16 14:04:27 -0500

    Undo unintended changes to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -58,7 +58,7 @@ export function Header() {
         <div className='flex items-center justify-between'>
           <Link href='/' className='relative text-2xl font-bold'>
             Kabeiri
-            <span className='absolute -bottom-4 right-0 rounded-full rounded-tl-none bg-gradient-to-r from-purple-500 to-purple-600 px-2 py-0.5 text-xs font-semibold text-white'>
+            <span className='absolute -bottom-4 right-0 rounded-full rounded-tl-none bg-gradient-to-b from-purple-600 to-pink-600 px-2 py-0.5 text-xs font-semibold text-white'>
               BETA
             </span>
           </Link>
@@ -102,14 +102,14 @@ export function Header() {
             </Link>
           ) : (
             <Link href='/login'>
-              <Button variant='secondary' size='sm'>
+              <Button variant='primary' size='sm'>
                 Login
               </Button>
             </Link>
           )}
         </div>
       </nav>
-      <div className='flex cursor-progress items-center justify-center bg-gradient-to-r from-purple-500 to-purple-600 py-2'>
+      <div className='flex cursor-progress items-center justify-center bg-gradient-to-b from-purple-600 to-pink-600 py-2'>
         <span className='font-bold text-white'>
           🚧 This site is under construction 🚧
         </span>
