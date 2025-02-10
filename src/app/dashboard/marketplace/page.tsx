eccc733f443679f074a269dbd51ac7c7d8610f6b
Zaker Choudhury <20971850+KaBankz@users.noreply.github.com>
2025-02-10 11:19:58 -0500

    Stub out other pages

---
+++ src/app/dashboard/marketplace/page.tsx
@@ -0,0 +1,10 @@
+export default function MarketplacePage() {
+  return (
+    <main className='p-8'>
+      <div className='mx-auto max-w-7xl'>
+        <h1 className='mb-8 text-3xl font-bold'>Marketplace</h1>
+        {/* Content will go here */}
+      </div>
+    </main>
+  );
+}
