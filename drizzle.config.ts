9592e99302a54bb17103d811bdd6863aaff8ee35
Zaker Choudhury <20971850+KaBankz@users.noreply.github.com>
2025-02-20 13:57:35 -0500

    Setup drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+
+import type { Config } from 'drizzle-kit';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('Missing DATABASE_URL');
+}
+
+export default {
+  schema: './src/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: { url: process.env.DATABASE_URL },
+  casing: 'snake_case',
+} satisfies Config;
