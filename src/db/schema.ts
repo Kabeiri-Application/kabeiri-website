caf1b13fcae32d9b03ae4fb90b9d2ed147eb300b
Zaker Choudhury <20971850+KaBankz@users.noreply.github.com>
2025-02-25 12:54:54 -0500

    Update schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,28 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+import { pgTable, text, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import { authUsers } from 'drizzle-orm/supabase/rls';
 
-export const usersTable = pgTable('users', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+export const profilesTable = pgTable('profiles', {
+  id: uuid()
+    .primaryKey()
+    .references(() => authUsers.id),
+  updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+  deletedAt: timestamp({ withTimezone: true }),
+  username: text().notNull(),
+  fullName: text().notNull(),
+  avatarUrl: text().notNull(),
+  website: text().notNull(),
+});
+
+export const organizationsTable = pgTable('organizations', {
+  id: uuid().primaryKey().defaultRandom(),
+  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+  updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+  deletedAt: timestamp({ withTimezone: true }),
+  owner: uuid().references(() => authUsers.id),
+  businessName: text().notNull(),
+  streetAddress: varchar().notNull(),
+  city: text().notNull(),
+  state: text().notNull(),
+  zipCode: text().notNull(),
 });
