c9f0b70db0fd6323df75b5a225094125c775a185
KaBanks <20971850+KaBankz@users.noreply.github.com>
2025-03-04 11:23:14 -0500

    Add drizzle (#5)
    * Add drizzle deps
    
    * Setup drizzle
    
    * Update schema
    
    * Added additional tables to schema
    
    ---------
    
    Co-authored-by: Joshua Grossman <46975990+Josh1794@users.noreply.github.com>

---
+++ src/db/schema.ts
@@ -0,0 +1,82 @@
+import {
+  numeric,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from 'drizzle-orm/pg-core';
+import { authUsers } from 'drizzle-orm/supabase/rls';
+
+export const rolesEnum = pgEnum('role', ['admin', 'owner', 'customer', 'user']);
+
+export const profilesTable = pgTable('profiles', {
+  id: uuid()
+    .primaryKey()
+    .references(() => authUsers.id),
+  updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+  deletedAt: timestamp({ withTimezone: true }),
+  username: text().notNull(),
+  fullName: text().notNull(),
+  avatarUrl: text().notNull(),
+  role: rolesEnum().notNull(),
+  organization: uuid().references(() => organizationsTable.id),
+  phone: text().notNull(),
+  streetAddress: varchar().notNull(),
+  city: text().notNull(),
+  state: text().notNull(),
+  zipCode: text().notNull(),
+});
+
+export const organizationsTable = pgTable('organizations', {
+  id: uuid().primaryKey().defaultRandom(),
+  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+  updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+  deletedAt: timestamp({ withTimezone: true }),
+  businessName: text().notNull(),
+  streetAddress: varchar().notNull(),
+  city: text().notNull(),
+  state: text().notNull(),
+  zipCode: text().notNull(),
+  website: text(),
+});
+
+export const carsTable = pgTable('cars', {
+  id: uuid().primaryKey().defaultRandom(),
+  owner: uuid().references(() => profilesTable.id),
+  make: text().notNull(),
+  model: text().notNull(),
+  year: text().notNull(),
+  vin: text().notNull(),
+  licensePlate: text().notNull(),
+  color: text().notNull(),
+});
+
+export const jobsTable = pgTable('jobs', {
+  id: uuid().primaryKey().defaultRandom(),
+  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+  updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+  deletedAt: timestamp({ withTimezone: true }),
+  customer: uuid().references(() => profilesTable.id),
+  organization: uuid().references(() => organizationsTable.id),
+  title: text().notNull(),
+  service: uuid().references(() => servicesTable.id),
+  description: text().notNull(),
+  status: text().notNull(),
+  dueDate: timestamp({ withTimezone: true }),
+  assignedTo: uuid().references(() => profilesTable.id),
+  createdBy: uuid().references(() => profilesTable.id),
+});
+
+export const servicesTable = pgTable('services', {
+  id: uuid().primaryKey().defaultRandom(),
+  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+  updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+  deletedAt: timestamp({ withTimezone: true }),
+  organization: uuid().references(() => organizationsTable.id),
+  title: text().notNull(),
+  description: text().notNull(),
+  price: numeric().notNull(),
+});
