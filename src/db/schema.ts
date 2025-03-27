ce623088e6b8b12f400bc374f609e48c7d0c8c41
Zaker Choudhury <20971850+KaBankz@users.noreply.github.com>
2025-03-27 12:26:07 -0400

    Add better auth schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,91 +1,132 @@
-import {
-  numeric,
-  pgEnum,
-  pgTable,
-  text,
-  timestamp,
-  uuid,
-  varchar,
-} from 'drizzle-orm/pg-core';
-import { authUsers } from 'drizzle-orm/supabase/rls';
+import { boolean, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 
-export const rolesEnum = pgEnum('role', ['admin', 'owner', 'customer', 'user']);
-export const jobStatusEnum = pgEnum('status', [
-  'complete',
-  'in progress',
-  'pending',
-]);
-
-export const profilesTable = pgTable('profiles', {
-  id: uuid()
-    .primaryKey()
-    .references(() => authUsers.id),
-  updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-  deletedAt: timestamp({ withTimezone: true }),
-  username: text().notNull(),
-  firstName: text().notNull(),
-  lastName: text().notNull(),
-  avatarUrl: text().notNull(),
-  role: rolesEnum().notNull(),
-  organization: uuid().references(() => organizationsTable.id),
-  phone: text().notNull(),
-  streetAddress: varchar().notNull(),
-  city: text().notNull(),
-  state: text().notNull(),
-  zipCode: text().notNull(),
+export const user = pgTable('user', {
+  id: text('id').primaryKey(),
+  name: text('name').notNull(),
+  email: text('email').notNull().unique(),
+  emailVerified: boolean('email_verified').notNull(),
+  image: text('image'),
+  createdAt: timestamp('created_at').notNull(),
+  updatedAt: timestamp('updated_at').notNull(),
 });
 
-export const organizationsTable = pgTable('organizations', {
-  id: uuid().primaryKey().defaultRandom(),
-  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-  updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-  deletedAt: timestamp({ withTimezone: true }),
-  businessName: text().notNull(),
-  businessPhotoUrl: text().notNull(),
-  streetAddress: varchar().notNull(),
-  city: text().notNull(),
-  state: text().notNull(),
-  zipCode: text().notNull(),
-  phone: text().notNull(),
-  website: text(),
+export const session = pgTable('session', {
+  id: text('id').primaryKey(),
+  expiresAt: timestamp('expires_at').notNull(),
+  token: text('token').notNull().unique(),
+  createdAt: timestamp('created_at').notNull(),
+  updatedAt: timestamp('updated_at').notNull(),
+  ipAddress: text('ip_address'),
+  userAgent: text('user_agent'),
+  userId: text('user_id')
+    .notNull()
+    .references(() => user.id, { onDelete: 'cascade' }),
 });
 
-export const carsTable = pgTable('cars', {
-  id: uuid().primaryKey().defaultRandom(),
-  owner: uuid().references(() => profilesTable.id),
-  make: text().notNull(),
-  model: text().notNull(),
-  year: text().notNull(),
-  vin: text().notNull(),
-  licensePlate: text().notNull(),
-  color: text().notNull(),
+export const account = pgTable('account', {
+  id: text('id').primaryKey(),
+  accountId: text('account_id').notNull(),
+  providerId: text('provider_id').notNull(),
+  userId: text('user_id')
+    .notNull()
+    .references(() => user.id, { onDelete: 'cascade' }),
+  accessToken: text('access_token'),
+  refreshToken: text('refresh_token'),
+  idToken: text('id_token'),
+  accessTokenExpiresAt: timestamp('access_token_expires_at'),
+  refreshTokenExpiresAt: timestamp('refresh_token_expires_at'),
+  scope: text('scope'),
+  password: text('password'),
+  createdAt: timestamp('created_at').notNull(),
+  updatedAt: timestamp('updated_at').notNull(),
 });
 
-export const jobsTable = pgTable('jobs', {
-  id: uuid().primaryKey().defaultRandom(),
-  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-  updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-  deletedAt: timestamp({ withTimezone: true }),
-  customer: uuid().references(() => profilesTable.id),
-  vehicle: uuid().references(() => carsTable.id),
-  organization: uuid().references(() => organizationsTable.id),
-  title: text().notNull(),
-  service: uuid().references(() => servicesTable.id),
-  description: text().notNull(),
-  status: jobStatusEnum().notNull().default('pending'),
-  dueDate: timestamp({ withTimezone: true }),
-  assignedTo: uuid().references(() => profilesTable.id),
-  createdBy: uuid().references(() => profilesTable.id),
+export const verification = pgTable('verification', {
+  id: text('id').primaryKey(),
+  identifier: text('identifier').notNull(),
+  value: text('value').notNull(),
+  expiresAt: timestamp('expires_at').notNull(),
+  createdAt: timestamp('created_at'),
+  updatedAt: timestamp('updated_at'),
 });
 
-export const servicesTable = pgTable('services', {
-  id: uuid().primaryKey().defaultRandom(),
-  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-  updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-  deletedAt: timestamp({ withTimezone: true }),
-  organization: uuid().references(() => organizationsTable.id),
-  title: text().notNull(),
-  description: text().notNull(),
-  price: numeric().notNull(),
-});
+// export const rolesEnum = pgEnum('role', ['admin', 'owner', 'customer', 'user']);
+// export const jobStatusEnum = pgEnum('status', [
+//   'complete',
+//   'in progress',
+//   'pending',
+// ]);
+
+// export const profilesTable = pgTable('profiles', {
+//   id: uuid()
+//     .primaryKey()
+//     .references(() => authUsers.id),
+//   updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+//   createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+//   deletedAt: timestamp({ withTimezone: true }),
+//   username: text().notNull(),
+//   firstName: text().notNull(),
+//   lastName: text().notNull(),
+//   avatarUrl: text().notNull(),
+//   role: rolesEnum().notNull(),
+//   organization: uuid().references(() => organizationsTable.id),
+//   phone: text().notNull(),
+//   streetAddress: varchar().notNull(),
+//   city: text().notNull(),
+//   state: text().notNull(),
+//   zipCode: text().notNull(),
+// });
+
+// export const organizationsTable = pgTable('organizations', {
+//   id: uuid().primaryKey().defaultRandom(),
+//   createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+//   updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+//   deletedAt: timestamp({ withTimezone: true }),
+//   businessName: text().notNull(),
+//   businessPhotoUrl: text().notNull(),
+//   streetAddress: varchar().notNull(),
+//   city: text().notNull(),
+//   state: text().notNull(),
+//   zipCode: text().notNull(),
+//   phone: text().notNull(),
+//   website: text(),
+// });
+
+// export const carsTable = pgTable('cars', {
+//   id: uuid().primaryKey().defaultRandom(),
+//   owner: uuid().references(() => profilesTable.id),
+//   make: text().notNull(),
+//   model: text().notNull(),
+//   year: text().notNull(),
+//   vin: text().notNull(),
+//   licensePlate: text().notNull(),
+//   color: text().notNull(),
+// });
+
+// export const jobsTable = pgTable('jobs', {
+//   id: uuid().primaryKey().defaultRandom(),
+//   createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+//   updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+//   deletedAt: timestamp({ withTimezone: true }),
+//   customer: uuid().references(() => profilesTable.id),
+//   vehicle: uuid().references(() => carsTable.id),
+//   organization: uuid().references(() => organizationsTable.id),
+//   title: text().notNull(),
+//   service: uuid().references(() => servicesTable.id),
+//   description: text().notNull(),
+//   status: jobStatusEnum().notNull().default('pending'),
+//   dueDate: timestamp({ withTimezone: true }),
+//   assignedTo: uuid().references(() => profilesTable.id),
+//   createdBy: uuid().references(() => profilesTable.id),
+// });
+
+// export const servicesTable = pgTable('services', {
+//   id: uuid().primaryKey().defaultRandom(),
+//   createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+//   updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+//   deletedAt: timestamp({ withTimezone: true }),
+//   organization: uuid().references(() => organizationsTable.id),
+//   title: text().notNull(),
+//   description: text().notNull(),
+//   price: numeric().notNull(),
+// });
