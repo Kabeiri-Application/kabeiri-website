c68bda3aafa06a6ddba8c1b64ad7e31fef90d908
Joshua Grossman <46975990+Josh1794@users.noreply.github.com>
2025-04-01 12:18:57 -0400

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,4 +17,4 @@ export async function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
-export const config = { matcher: ['/dashboard', '/account'] };
+export const config = { matcher: ['/dashboard'] };
