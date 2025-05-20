10f0796adf8e6928459d5f3ad056deac6ba0acc2
Joshua Grossman <46975990+Josh1794@users.noreply.github.com>
2025-05-20 14:33:51 -0400

    styling changes

--- src/app/dashboard/services/_components/service-card.tsx
+++ src/app/dashboard/services/_components/service-card.tsx
@@ -1,24 +1,30 @@
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation';
 
-import type { Service } from "@/db/app.schema";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import type { Service } from '@/db/app.schema';
 
 export function ServiceCard(service: Service) {
   const router = useRouter();
   return (
-    <div
+    <Card
       onClick={() => router.push(`/dashboard/services/${service.id}`)}
       key={service.id}
-      className="group relative cursor-pointer rounded-lg border border-gray-200 bg-white p-6 shadow-sm transition-all hover:shadow-md"
-    >
-      <div className="mb-4 flex items-center justify-between">
-        <div className="flex items-center">
-          <h2 className="text-xl font-semibold text-gray-900">
-            {service.title}
-          </h2>
-        </div>
-      </div>
-      <p className="text-gray-600">Description: {service.description}</p>
-      <p className="text-gray-600">{`Price: $${service.price}`}</p>
-    </div>
+      className='group relative cursor-pointer transition-all hover:border-white'>
+      <CardHeader>
+        <CardTitle>{service.title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>
+          <p>Description: {service.description}</p>
+          <p>{`Price: $${service.price}`}</p>
+        </CardDescription>
+      </CardContent>
+    </Card>
   );
 }
