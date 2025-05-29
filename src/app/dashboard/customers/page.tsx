b6a31ce7c2e7ed1417e9dc502f0aaf482abd0ff0
Joshua Grossman <46975990+Josh1794@users.noreply.github.com>
2025-05-29 11:19:31 -0400

    removed link to vehicles page and added customers data

--- src/app/dashboard/customers/page.tsx
+++ src/app/dashboard/customers/page.tsx
@@ -1,9 +1,62 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { getCustomers } from "@/app/dashboard/customers/actions";
+import { getOrganizationId } from "@/app/dashboard/jobs/actions";
+import { type Customer } from "@/app/dashboard/jobs/schema";
+
 export default function Page() {
+  const [customers, setCustomers] = useState<Customer[]>([]);
+  const [organization, setOrganization] = useState("");
+
+  const fetchData = async () => {
+    try {
+      const organizationId = await getOrganizationId();
+      if (!organizationId) {
+        throw new Error("Organization ID not found");
+      }
+      setOrganization(organizationId);
+
+      const customers = await getCustomers(organizationId);
+      if (!customers) {
+        throw new Error("No customers found for the organization");
+      }
+
+      setCustomers(customers);
+    } catch (error) {
+      console.error("Failed to fetch data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log("Customers:", customers);
+
   return (
     <main className="p-8">
       <div className="mx-auto max-w-7xl">
         <div className="mb-8 flex items-center justify-between">
           <h1 className="text-3xl font-bold">Customers</h1>
+        </div>
+        <div className="grid grid-cols-1 gap-6">
+          {customers.length > 0 ? (
+            customers.map((customer) => (
+              <div
+                key={customer.id}
+                className="rounded-lg border p-4 shadow-sm transition-shadow hover:cursor-pointer hover:shadow-md"
+              >
+                <h2 className="text-xl font-semibold">
+                  {customer.firstName + " " + customer.lastName}
+                </h2>
+                <p>{customer.streetAddress}</p>
+              </div>
+            ))
+          ) : (
+            <p className="text-gray-500">No customers found.</p>
+          )}
         </div>
       </div>
     </main>
