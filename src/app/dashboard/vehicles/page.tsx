a22ec070653f6773cad08866732500312d6632c0
Joshua Grossman <46975990+Josh1794@users.noreply.github.com>
2025-05-29 10:57:50 -0400

    adding actions and titles

--- src/app/dashboard/vehicles/page.tsx
+++ src/app/dashboard/vehicles/page.tsx
@@ -1,7 +1,11 @@
 export default function Page() {
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      <h1 className="text-2xl font-bold">Vehicles Page</h1>
-    </div>
+    <main className="p-8">
+      <div className="mx-auto max-w-7xl">
+        <div className="mb-8 flex items-center justify-between">
+          <h1 className="text-3xl font-bold">Vehicles</h1>
+        </div>
+      </div>
+    </main>
   );
 }
