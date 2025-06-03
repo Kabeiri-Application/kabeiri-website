ad38e1d5565dc814a13461cdc728a784c696b4cd
Joshua Grossman <46975990+Josh1794@users.noreply.github.com>
2025-06-03 10:42:32 -0400

    added customer actions

--- src/app/dashboard/customers/actions.ts
+++ src/app/dashboard/customers/actions.ts
@@ -1,9 +1,12 @@
 "use server";
 
+import { headers } from "next/headers";
+
 import { and, eq } from "drizzle-orm";
 
 import { db } from "@/db";
-import { profilesTable } from "@/db/app.schema";
+import { NewProfile, profilesTable } from "@/db/app.schema";
+import { auth } from "@/lib/auth";
 
 export async function getCustomers(organizationId: string) {
   try {
@@ -15,5 +18,40 @@ export async function getCustomers(organizationId: string) {
     });
   } catch (error) {
     console.error("Error in getCustomers:", error);
+  }
+}
+
+export async function getCustomer(customerId: string) {
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session?.user?.id) {
+    console.error("Not authenticated");
+  }
+
+  try {
+    const service = await db.query.profilesTable.findFirst({
+      where: eq(profilesTable.id, customerId),
+    });
+    return service;
+  } catch (error) {
+    console.error("Error in getCustomer:", error);
+  }
+}
+
+export async function editCustomer(formData: NewProfile, customerId: string) {
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session?.user?.id) {
+    console.error("Not authenticated");
+  }
+  try {
+    console.log("Editing service with ID:", customerId);
+    await db
+      .update(profilesTable)
+      .set({
+        ...formData,
+      })
+      .where(eq(profilesTable.id, customerId));
+    return { success: true };
+  } catch (error) {
+    console.error("Error in editCustomer:", error);
   }
 }
