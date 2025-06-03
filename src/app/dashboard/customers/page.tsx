c6ed6a2ba441c0dc42888aa6a559db7309acf801
Joshua Grossman <46975990+Josh1794@users.noreply.github.com>
2025-06-03 10:43:33 -0400

    chnaged to profile type and added componentized card

--- src/app/dashboard/customers/page.tsx
+++ src/app/dashboard/customers/page.tsx
@@ -3,11 +3,12 @@
 import { useEffect, useState } from "react";
 
 import { getCustomers } from "@/app/dashboard/customers/actions";
+import { CustomerCard } from "@/app/dashboard/customers/components/customer-card";
 import { getOrganizationId } from "@/app/dashboard/jobs/actions";
-import { type Customer } from "@/app/dashboard/jobs/schema";
+import { Profile } from "@/db/app.schema";
 
 export default function Page() {
-  const [customers, setCustomers] = useState<Customer[]>([]);
+  const [customers, setCustomers] = useState<Profile[]>([]);
   const [organization, setOrganization] = useState("");
 
   const fetchData = async () => {
@@ -44,15 +45,7 @@ export default function Page() {
         <div className="grid grid-cols-1 gap-6">
           {customers.length > 0 ? (
             customers.map((customer) => (
-              <div
-                key={customer.id}
-                className="rounded-lg border p-4 shadow-sm transition-shadow hover:cursor-pointer hover:shadow-md"
-              >
-                <h2 className="text-xl font-semibold">
-                  {customer.firstName + " " + customer.lastName}
-                </h2>
-                <p>{customer.streetAddress}</p>
-              </div>
+              <CustomerCard key={customer.id} {...customer} />
             ))
           ) : (
             <p>No customers found.</p>
