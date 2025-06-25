7796197f6d228af146f1dcb2e4f54cbe4847c76c
Joshua Grossman <46975990+Josh1794@users.noreply.github.com>
2025-06-25 10:22:38 -0400

    settings page concept

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -12,42 +12,29 @@ export default async function Page() {
         <div className="mb-8 flex items-center justify-between">
           <h1 className="text-3xl font-bold">Settings</h1>
         </div>
-        <div className="rounded-lg bg-white p-6 shadow">
+        <div className="mt-2 rounded-lg bg-white p-6 shadow">
           <h2 className="mb-4 text-xl font-semibold">Account Settings</h2>
           <p className="text-muted-foreground mb-2">
             Update your profile information, change your password, and manage
             your account preferences.
           </p>
-          <form>
-            <div className="mt-6">
-              <label className="block text-sm font-medium">Email</label>
-              <input
-                defaultValue={session?.user?.email || ""}
-                // {...register("make")}
-                placeholder="Email"
-                className="mt-1 block w-full rounded-lg border border-gray-300 px-3 py-2 shadow-sm focus:border-green-700 focus:ring-2 focus:ring-green-700 focus:outline-none"
-              />
-              {/* {errors.make && (
-              <span className="text-sm text-red-500">
-                {errors.make.message}
-              </span>
-            )} */}
-            </div>
-            <div className="mt-6">
-              <label className="block text-sm font-medium">Name</label>
-              <input
-                defaultValue={session?.user?.name || ""}
-                placeholder="Name"
-                className="mt-1 block w-full rounded-lg border border-gray-300 px-3 py-2 shadow-sm focus:border-green-700 focus:ring-2 focus:ring-green-700 focus:outline-none"
-              />
-              {/* {errors.model && (
-              <span className="text-sm text-red-500">
-                {errors.model.message}
-              </span>
-            )} */}
-            </div>
-            <Button className="mt-4">Update Profile</Button>
-          </form>
+          <Button>Update Account</Button>
+        </div>
+
+        <div className="mt-2 rounded-lg bg-white p-6 shadow">
+          <h2 className="mb-4 text-xl font-semibold">Privacy and Security</h2>
+          <p className="text-muted-foreground mb-2">
+            Update your accounts security and privacy preferences.
+          </p>
+          <Button>Update Security</Button>
+        </div>
+
+        <div className="mt-2 rounded-lg bg-white p-6 shadow">
+          <h2 className="mb-4 text-xl font-semibold">Accessability</h2>
+          <p className="text-muted-foreground mb-2">
+            Update your accessability preferences.
+          </p>
+          <Button>Update Accessability</Button>
         </div>
       </div>
     </main>
