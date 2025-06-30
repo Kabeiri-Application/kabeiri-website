2fbcc60dd4dd554f7fab79df692a5c9be8c39af0
Joshua Grossman <46975990+Josh1794@users.noreply.github.com>
2025-06-30 14:34:40 -0400

    added a redirect on the admin settings page

--- src/app/dashboard/settings/adminSettings/page.tsx
+++ src/app/dashboard/settings/adminSettings/page.tsx
@@ -1,4 +1,5 @@
 import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 import { auth } from "@/lib/auth";
 
@@ -7,6 +8,11 @@ import SettingPage from "../components/settingPage";
 export default async function Page() {
   const session = await auth.api.getSession({ headers: await headers() });
   console.log("Session:", session?.user?.role);
+
+  if (!session?.user?.role || session.user.role !== "admin") {
+    redirect("/error");
+  }
+
   return (
     <SettingPage title="Admin Settings">
       <p>Admin settings will be implemented here.</p>
