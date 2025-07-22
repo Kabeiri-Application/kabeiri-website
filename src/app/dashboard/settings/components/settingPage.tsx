b20e6f886754cf7d5cd01543ab69d3886426ee73
Joshua Grossman <46975990+Josh1794@users.noreply.github.com>
2025-07-22 10:21:41 -0400

    updated settingspage back button

--- src/app/dashboard/settings/components/settingPage.tsx
+++ src/app/dashboard/settings/components/settingPage.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 import { ArrowLeftIcon } from "lucide-react";
 
@@ -11,18 +9,16 @@ export default function SettingPage({
   title: string;
   children: React.ReactNode;
 }) {
-  const router = useRouter();
-
   return (
     <main className="p-8">
       <div className="mx-auto max-w-7xl">
-        <button
-          onClick={() => router.back()}
+        <Link
+          href="/dashboard/settings"
           className="hover:text-primary mb-8 flex cursor-pointer items-center"
         >
           <ArrowLeftIcon className="size-4" />
           Back to Settings
-        </button>
+        </Link>
         <div className="mb-8 flex items-center justify-between">
           <h1 className="text-3xl font-bold">{title}</h1>
         </div>
