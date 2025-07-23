a15ed57b33c4c2f39029b79c8be49cb30dad95b0
Joshua Grossman <46975990+Josh1794@users.noreply.github.com>
2025-07-23 11:46:25 -0400

    Adding polar config

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,9 @@
+import { polarClient } from "@polar-sh/better-auth";
 import { adminClient, organizationClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  plugins: [adminClient(), organizationClient()],
+  plugins: [adminClient(), organizationClient(), polarClient()],
 });
 
 export const { signIn, signUp, useSession } = createAuthClient();
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,11 @@
+import {
+  checkout,
+  polar,
+  portal,
+  usage,
+  // webhooks,
+} from "@polar-sh/better-auth";
+import { Polar } from "@polar-sh/sdk";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
@@ -5,6 +13,14 @@ import { admin, organization } from "better-auth/plugins";
 
 import { db } from "@/db";
 import { env } from "@/env";
+
+const polarClient = new Polar({
+  accessToken: process.env.POLAR_DEV_TOKEN,
+  // Use 'sandbox' if you're using the Polar Sandbox environment
+  // Remember that access tokens, products, etc. are completely separated between environments.
+  // Access tokens obtained in Production are for instance not usable in the Sandbox environment.
+  server: "sandbox",
+});
 
 export const auth = betterAuth({
   baseUrl: env.BETTER_AUTH_URL,
@@ -15,5 +31,34 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  plugins: [admin(), nextCookies(), organization()],
+  plugins: [
+    admin(),
+    nextCookies(),
+    organization(),
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "cdc7df74-d1fc-442a-b135-a9509ded3315", // ID of Product from Polar Dashboard
+              slug: "free", // Custom slug for easy reference in Checkout URL, e.g. /checkout/pro
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+        usage(),
+        // webhooks({
+        //     secret: process.env.POLAR_WEBHOOK_SECRET,
+        //     onCustomerStateChanged: (payload) => // Triggered when anything regarding a customer changes
+        //     onOrderPaid: (payload) => // Triggered when an order was paid (purchase, subscription renewal, etc.)
+        //     ...  // Over 25 granular webhook handlers
+        //     onPayload: (payload) => // Catch-all for all events
+        // })
+      ],
+    }),
+  ],
 });
